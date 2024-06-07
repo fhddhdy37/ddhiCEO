abcaa6eaa20112cbb6cf7f606a9f9bd075d809a1
fhddhdy37 <fhddhdy37@naver.com>
2024-06-07 21:24:18 +0900

    Set Firebase settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.github.yuriy-budiyev:code-scanner:2.3.2")
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database-ktx")
 }
